e7222e4ceed7d6ed315a4d2d7740e0f5deb9cc10
Andrew Shankie <andrew@properdesign.rs>
2015-09-28 09:57:46 +0100

    Adds WP_Session as a submodule

---
+++ eo-confirmation-page-session.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Plugin Name: EO Confirmation Page Session
+ * Description: Stores user's last completed Event Oragniser booking in a WP_Session to allow access to booking meta on a confirmation page.
+ * Version: 0.1
+ * Author: Proper Design
+ * Author URI: http://properdesign.rs
+ * License: GPL2
+ *
+ * Acknowledgements: 
+ * Eric Mann's WP_Session plugin https://github.com/ericmann/wp-session-manager
+ * Stephen Harris and Event Organiser for providing help getting this going http://wp-event-organiser.com
+ */
+
+/*  Copyright 2015  Proper Design  (email : hello@properdesign.rs)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
