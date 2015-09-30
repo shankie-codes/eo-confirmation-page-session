c36205cb49b5dea92a3159230d7768eba33627be
Andrew Shankie <andrew@properdesign.rs>
2015-09-30 17:12:37 +0100

    Removes inline styles from template

--- templates/eo-booking-confirmation.php
+++ templates/eo-booking-confirmation.php
@@ -27,7 +27,33 @@ if($booking_id):
   </div>
 
   <div class="confirmation-ticket-table">
-    <?php echo eventorganiser_email_ticket_list($booking_id, 'eo-email-template-clwb.php'); ?>  
+    <?php 
+    // Based on the code of eventorganiser_email_ticket_list()
+    
+    $booking_tickets = eo_get_booking_tickets( $booking_id, false );
+    $total_price = eo_get_booking_meta( $booking_id, 'booking_amount' );
+    
+
+    $booking_table = sprintf(
+      '<table>
+      <thead><tr> <th>%s</th><th> %s </th></tr></thead>
+      <tbody>',
+      __( 'Ticket', 'eventorganiserp' ),
+      __( 'Price', 'eventorganiserp' )
+    );
+
+    foreach ( $booking_tickets as $ticket ) {
+      $booking_table .= sprintf(
+        '<tr> <td>%s<td> %s </td></tr>',
+        esc_html( $ticket->ticket_name ),
+        eo_format_price( $ticket->ticket_price )
+      );
+    }
+
+    $booking_table .= sprintf( '<tr> <td>%s</td><td> %s </td> <td></td></tr></tbody></table>', __( 'Total', 'eventorganiserp' ), eo_format_price( $total_price ) );
+
+    echo $booking_table;
+    ?>  
   </div>
 
 <?php else: ?>
